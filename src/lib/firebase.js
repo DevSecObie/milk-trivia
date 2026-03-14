976a37cf96f7aa26ade8d6b435db2e3cf87f8fdf
root <root@srv1441094.hstgr.cloud>
2026-03-14 07:00:33 +0000

    chore: switch authDomain to milk.cyberjudah.io

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || 'AIzaSyAAjHl2zi4gMaa70RgMaE8GTUQF3mO--8M',
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'milk-17053.firebaseapp.com',
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || 'milk.cyberjudah.io',
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || 'milk-17053',
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || 'milk-17053.firebasestorage.app',
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || '792873745635',
